ca148765e51eb4d009f27040bcb295fe24a9fec4
Richard Forth <richard.forth@gmail.com>
2020-12-11 15:44:07 +0000

    Create simplevar.c

---
+++ simplevar.c
@@ -0,0 +1,22 @@
+/* Simple Variable printer
+*
+*  Author:   Richard A. Forth
+*  Date:     2020-12-11
+*  Does:     Prints a variable value to screen
+*  Compiler: CodeBlocks
+*  OS:       Windows 10
+*
+*/
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+void main()
+{
+    int a=100;
+    system("cls");
+    printf("%d\n", a);
+    getch();
+
+}

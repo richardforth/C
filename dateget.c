dba58b2d6f8e59b6104fc246c5ff3138d1367c54
Richard Forth <richard.forth@gmail.com>
2020-12-11 15:56:41 +0000

    Create dateget.c

---
+++ dateget.c
@@ -0,0 +1,13 @@
+/*
+    * program to get the current system date in windows
+*/
+
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+  time_t t = time(NULL);
+  struct tm tm = *localtime(&t);
+  printf("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+}

2834b5c92d942895b3a20290266c794fafa777ff
Richard Forth <richard.forth@gmail.com>
2020-12-12 00:45:10 +0000

    Create pands4.c

---
+++ pands4.c
@@ -0,0 +1,10 @@
+/* pands4.c */
+#include <stdio.h>
+int main()
+{
+    int minx, x, y, z;
+    printf("Enter four ints: ");
+    scanf( "%d %d %d %d", &minx, &x, &y, &z);
+    printf("You wrote: %d %d %d %d", minx, x, y, z);
+    return 0;
+}

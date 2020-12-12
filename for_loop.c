a91213d4a25a426dea088b9612c23436882c9901
Richard Forth <richard.forth@gmail.com>
2020-12-12 22:52:02 +0000

    Create for_loop.c

---
+++ for_loop.c
@@ -0,0 +1,19 @@
+/*  for_loop.c
+*
+*  Author:   Richard A. Forth
+*  Date:     2020-12-12
+*  Does:     count to 10 using for loop
+*  Compiler: CodeBlocks
+*  OS:       Windows 10
+*
+*/
+
+#include <stdio.h>
+
+void main()
+{
+    for(int i = 1; i <= 10; i++)
+    {
+        printf("%d\n", i);
+    }
+}

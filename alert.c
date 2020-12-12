1d51cafa6881dbe818bfab6d02bc697a19663977
Richard Forth <richard.forth@gmail.com>
2020-12-12 09:14:38 +0000

    Create alert.c

---
+++ alert.c
@@ -0,0 +1,16 @@
+/*  alert.c
+*
+*  Author:   Richard A. Forth
+*  Date:     2020-12-12
+*  Does:     Beeps and throws a message || Beep is windows notification || \a esc sequence character
+*  Compiler: CodeBlocks
+*  OS:       Windows 10
+*
+*/
+
+#include <stdio.h>
+
+void main()
+{
+   printf("\a** alert **\n");
+}

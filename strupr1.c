671d6944a3ca3775b37e0c1c89e84a5fdc14b648
Richard Forth <richard.forth@gmail.com>
2021-01-02 15:36:08 +0000

    Create strupr1.c

---
+++ strupr1.c
@@ -0,0 +1,26 @@
+/*  strupr1.c
+*
+*  Author:   Richard A. Forth
+*  Date:     2021-01-02
+*  Does:     Example of converting string to upper case || uses strupr() function from string.h
+*  Compiler: CodeBlocks
+*  OS:       Windows 10
+*
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+    char name1[140] = "world of computers";
+    char name2[140];
+    int c;
+
+    strcpy(name2, name1);
+    strupr(name2);
+
+    printf("The original string name1=[%s]\n", name1);
+    printf("The uppoer case version name2=[%s]\n", name2);
+
+}

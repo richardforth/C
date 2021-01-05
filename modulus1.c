26c17b528a358f90fd422a5483a24fd4256450a4
Richard Forth <richard.forth@gmail.com>
2021-01-05 15:54:33 +0000

    Create modulus1.c

---
+++ modulus1.c
@@ -0,0 +1,13 @@
+/* C CODE TO PRINT MODULUS 10 OF 12345 */
+
+#include <stdio.h>
+
+void main()
+{
+   int NUM = 12345, R = 0;
+
+   R = R + NUM/10;
+
+   printf("Modulus 10 of 12345 is = %d\n", R);
+
+}

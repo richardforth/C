3a940c8d3bddd701740282f960880860fa34861f
Richard Forth <richard.forth@gmail.com>
2021-01-05 15:11:35 +0000

    Create reversenum.c

---
+++ reversenum.c
@@ -0,0 +1,24 @@
+/* C CODE TO REVERSE 5 DIGIT NUMBER ENTERED THROUGH KEYBOARD */
+//The Number should be less than 32767
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+   int NUM, R = 0;
+   clrscr();
+
+   printf("Enter a number to reverse\n");
+   scanf("%d", &NUM);
+
+   while (NUM != 0)
+   {
+      R = R * 10;
+      R = R + NUM%10;
+      NUM= NUM/10;
+   }
+
+   printf("After reversing the Number is = %d\n", R);
+
+   getch();
+}

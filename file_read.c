f851b53aaeb5c8d577f0bfb78a5bf4a99d3d9a7d
Richard Forth <richard.forth@gmail.com>
2021-01-09 15:59:18 +0000

    Create file_read.c

---
+++ file_read.c
@@ -0,0 +1,35 @@
+/* file_read.c
+*
+*  Create a C program the write a new file or overwrites and existing file of the same name
+*
+*/
+
+
+#import <stdio.h>
+
+
+void main()
+{
+    FILE *fp;
+    char ch;
+
+    fp = fopen("H:\\Programming\\C\\file_new.txt", "r");
+
+    /* test for successful file open operation*/
+    if (fp == 0)
+    {
+        printf("Error--file could not be opened.\n");
+        exit(1);
+    }
+
+    /* retreive our message */
+    while(ch!=EOF)
+    {
+        ch=fgetc(fp);
+        printf("%c",ch);
+    }
+
+    /* always close files you've opened */
+    fclose(fp);
+
+}

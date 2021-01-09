46a9053300001d7350a354fc94fe0ed8f1071c1e
Richard Forth <richard.forth@gmail.com>
2021-01-09 10:01:49 +0000

    Create primes100.c

---
+++ primes100.c
@@ -0,0 +1,33 @@
+/*  WRITE C CODE TO PRINT PRIME NUMBERS IN BETWEEN 1-100 USING BREAK AND CONTINUE  */
+
+#include <math.h>
+#include <stdio.h>
+
+void main()
+
+{
+  int i, j;
+  i = 2;
+  while ( i < 100 )
+{
+     j = 2;
+  while ( j < sqrt(i) )
+     {
+         if ( i % j == 0 )
+            break;
+
+         else
+         {
+            ++j;
+            continue;
+         }
+      }
+
+      if ( j > sqrt(i) )
+
+            printf("%d|", i);
+
+      ++i;
+  }
+
+}

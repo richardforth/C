78666eae63845624f40421c3dbdd4be9b8502ae8
Richard Forth <richard.forth@gmail.com>
2021-01-14 08:02:26 +0000

    Update setpass.c

--- setpass.c
+++ setpass.c
@@ -8,27 +8,62 @@
 /***************** so treat them as you would plain text files *********************/
 
 
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
 
 
 void main()
 {
     FILE *fp;
+    int i;
     char np[128];  // new password
     char cnp[128]; // confirm new password
 
-    /// work needed to hide inputs
-
+    /// hide inputs with stars (requires conio.h)
     printf("Enter a new password: ");
-    scanf("%s", &np);
+    for (i = 0; i < sizeof(np); i++) {
+
+        // Get the hidden input
+        // using getch() method
+        np[i] = _getch();
+
+
+        // check for enter key
+        if(np[i] == 13)
+            break;
+
+        // Print * to show that
+        // a character is entered
+        printf("*");
+
+    }
+    np[i] = '\0';
+    printf("\n");
 
     printf("Confirm new password: ");
-    scanf("%s", &cnp);
+    for (i = 0; i < sizeof(cnp); i++) {
+
+        // Get the hidden input
+        // using getch() method
+        cnp[i] = _getch();
+
+        // check for enter key
+        if(cnp[i] == 13)
+            break;
+
+        // Print * to show that
+        // a character is entered
+        printf("*");
+
+    }
+    cnp[i] = '\0';
+    printf("\n");
 
     if (strcmp(cnp, np) == 0)
     {
          fp=fopen("security.dat","wb+");
-         fwrite(&np,sizeof(cnp),1,fp);
+         fwrite(&np,sizeof(np),1,fp);
          fclose(fp);
          printf("OK, password updated successfully.");
     }

f14a51e71832de596c54e9df009d16e1f9ae9f39
Richard Forth <richard.forth@gmail.com>
2021-01-15 12:54:26 +0000

    Update addition.c

--- mathematics/addition.c
+++ mathematics/addition.c
@@ -1,3 +1,34 @@
-/* addition.c */
+/* addition.c
+*
+*  Author: Richard A. Forth
+*  Does:   Add numbers, until 0 is entered
+*  Date:   15-01-2021
+*  Comments:
+*     I am retaking mathematics and using c code for testing certain operations
+*
+*/
 
-// tba
+
+#include <string.h>
+#include <math.h> // not strictly needed for simple addition but useful for future math problems
+
+void main()
+{
+    int num;
+    int total = 0;
+
+
+    printf("[ %d ] Add number (0 to end input): ", total);
+    scanf("%d", &num);
+
+
+    while( num != 0)
+    {
+        total += num;
+        printf("[ %d ] Add number (0 to end input): ", total);
+        scanf("%d", &num);
+    }
+
+    printf("Total: %d", total);
+
+}

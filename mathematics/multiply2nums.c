0dadde9e364dc5192be2509dffbe287767bfb5a7
Richard Forth <richard.forth@gmail.com>
2021-01-16 07:48:12 +0000

    Create multiply2nums.c

---
+++ mathematics/multiply2nums.c
@@ -0,0 +1,20 @@
+/* multiply2nums.c */
+
+
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    int a, b, sum;
+
+    printf("First number: ");
+    scanf("%d", &a);
+    printf("Second number: ");
+    scanf("%d", &b);
+
+    sum = a * b;
+    printf("Result: %d", sum);
+
+
+}

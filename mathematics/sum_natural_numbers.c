749d66499b3259bd452de9400999983d0f6ac5ef
Richard Forth <richard.forth@gmail.com>
2021-01-19 15:58:14 +0000

    Create sum_natural_numbers.c

---
+++ mathematics/sum_natural_numbers.c
@@ -0,0 +1,31 @@
+/*  sum_natural_numbers.c
+*   Credit: https://www.programiz.com/c-programming/c-recursion
+*/
+
+#include <stdio.h>
+int sum(int n);
+
+int main() {
+    int number, result;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &number);
+
+    result = sum(number);
+
+    printf("sum = %d", result);
+    return 0;
+}
+
+int sum(int n) {
+
+    if (n != 0)
+    {
+        // sum() function calls itself
+        return  n + sum(n-1);
+    }
+    else
+    {
+       return n;
+    }
+}

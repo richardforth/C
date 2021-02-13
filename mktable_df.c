87a36d11576e442908dfb8d09b405a9f1bc117cd
Richard Forth <richard.forth@gmail.com>
2021-02-13 19:41:33 +0000

    Update mktable_df.c

--- mktable_df.c
+++ mktable_df.c
@@ -4,7 +4,8 @@
 
 // attribution: https://cmd.inp.nsk.su/old/cmd2/manuals/unix/UNIX_Unleashed/ch08.htm
 
-// Usage: df -h > df_tab && ./mktable_df df_tab | tbl | nroff | grep -v "^$"
+// Usage:
+// df -h > df_tab && sed -i 's/-/\\-/g' df_tab && sed -i 's/\s\+/\t/g' -i df_tab && sed -i 's/Mounted\ton/Mounted on/' df_tab &&  ./rfmktable_df df_tab | tbl | nroff | grep -v "^$"
 
 int main ( int argc, char *argv[] )
 {
@@ -19,26 +20,26 @@ int main ( int argc, char *argv[] )
         fname = argv[1];
         if( ! access( fname, R_OK ) == 0 ) {
                 // file doesn't exist
-                printf("\nError: Too many arguments supplied!\n");
+                printf("\nError: File does't exist or we can't read it!\n");
                 return 1;
         }
    }
    else
    {
-        printf("\nError: Too many arguments supplied!\n");
+        printf("\nError: Wrong number of arguments supplied!\n");
         return 1;
    }
 
    fp = fopen(fname, "r");
 
    printf("\\\" This file produced by a program!\n");
+   printf(".ll 11i\n");
+   printf(".in 1i\n");
    printf(".TS \n");
    printf("allbox;\n");
-   printf("c s s\n");
-   printf("cB cB cB cB cB cB cB\n");
+   printf("c c c c c c c\n");
    printf("l l l l l l l .\n");
    // do something to print tab or space delimited data
-   printf("DISK SPACE REPORT\n");
    while  ( (ch = fgetc(fp)) != EOF)
     {
         printf("%c",ch);

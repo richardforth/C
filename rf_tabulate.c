5a3c43a8667ad1bb32541c7be792da6b06e951d6
Richard Forth <richard.forth@gmail.com>
2021-02-13 15:17:48 +0000

    Update rf_tabulate.c

--- rf_tabulate.c
+++ rf_tabulate.c
@@ -1,30 +1,9 @@
-$ cat rf_tabulate.c
 #include <stdio.h>
 #include <stdlib.h>
 
 // attribution: https://cmd.inp.nsk.su/old/cmd2/manuals/unix/UNIX_Unleashed/ch08.htm
 
 // Usage: ./rf_tabulate | tbl | nroff | grep -v "^$"
-
-/* Produces the following table 
-┌─────────┬─────────────────┬─────────────────┐
-│Language │     Authors     │     Runs on     │
-├─────────┼─────────────────┼─────────────────┤
-│         │                 │                 │
-├─────────┼─────────────────┼─────────────────┤
-│Fortran  │ Many            │ Almost anything │
-├─────────┼─────────────────┼─────────────────┤
-│PL/1     │ IBM             │ 360/370         │
-├─────────┼─────────────────┼─────────────────┤
-│C        │ BTL             │ 11/45,H6000,370 │
-├─────────┼─────────────────┼─────────────────┤
-│BLISS    │ Carnegie−Mellon │ PDP−10,11       │
-├─────────┼─────────────────┼─────────────────┤
-│IDS      │ Honeywell       │ H6000           │
-├─────────┼─────────────────┼─────────────────┤
-│Pascal   │ Stanford        │ 370             │
-└─────────┴─────────────────┴─────────────────┘
-*/
 
 main ( )
 {
@@ -34,9 +13,13 @@ main ( )
    printf("\\\" This file produced by a program!\n");
    printf(".TS \n");
    printf("allbox;\n");
-   printf("c c c\n");
+   printf("c s s\n");
+   printf("cB cB cB\n");
    printf("l l l.\n");
    // do something to print tab or space delimited data
+   // eventually we will pull in a file so this program
+   // is more useful to us. Demo Data for now:
+   printf("Computer Programming Languages\n");
    printf("Language     Authors Runs on\n");
    printf("\n");
    printf("Fortran      Many    Almost anything\n");

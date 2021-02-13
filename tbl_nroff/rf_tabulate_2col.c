423d7945fbe16d7fb7856bba4e4d254715e19fb8
Richard Forth <richard.forth@gmail.com>
2021-02-13 21:39:18 +0000

    Create rf_tabulate_2col.c

---
+++ tbl_nroff/rf_tabulate_2col.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// attribution: https://cmd.inp.nsk.su/old/cmd2/manuals/unix/UNIX_Unleashed/ch08.htm
+
+// Usage: ./rf_tabulate | tbl | nroff | grep -v "^$"
+
+int main ( int argc, char *argv[] )
+{
+   int index;
+   FILE *fp;
+   char ch;
+   char *fname;
+
+   // get file name argument
+   if (argc == 2 )
+   {
+        fname = argv[1];
+        if( ! access( fname, R_OK ) == 0 ) {
+                // file doesn't exist
+                printf("\nError: Too many arguments supplied!\n");
+                return 1;
+        }
+   }
+   else
+   {
+        printf("\nError: Too many arguments supplied!\n");
+        return 1;
+   }
+
+   fp = fopen(fname, "r");
+
+   printf("\\\" This file produced by a program!\n");
+   printf(".ll 15i \n");
+   printf(".TS \n");
+   printf("allbox;\n");
+   printf("l l .\n");
+   // do something to print tab or space delimited data
+   while  ( (ch = fgetc(fp)) != EOF)
+    {
+        printf("%c",ch);
+    }
+   /* always close files you've opened */
+   fclose(fp);
+   // end raw data printing
+   printf(".TE");
+
+   return 0;
+}

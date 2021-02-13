27a080e9a5d9a7ae855402485527dbcbc326aa76
Richard Forth <richard.forth@gmail.com>
2021-02-13 15:04:09 +0000

    Create rf_tabulate.c

---
+++ rf_tabulate.c
@@ -0,0 +1,28 @@
+$ cat rf_tabulate.c
+#include <stdio.h>
+#include <stdlib.h>
+
+// attribution: https://cmd.inp.nsk.su/old/cmd2/manuals/unix/UNIX_Unleashed/ch08.htm
+
+main ( )
+{
+   int index;
+   char *data[1024];
+
+   printf("\\\" This file produced by a program!\n");
+   printf(".TS \n");
+   printf("allbox;\n");
+   printf("c c c\n");
+   printf("l l l.\n");
+   // do something to print tab or space delimited data
+   printf("Language     Authors Runs on\n");
+   printf("\n");
+   printf("Fortran      Many    Almost anything\n");
+   printf("PL/1 IBM     360/370\n");
+   printf("C    BTL     11/45,H6000,370\n");
+   printf("BLISS        Carnegie\\-Mellon       PDP\\-10,11\n");
+   printf("IDS  Honeywell       H6000\n");
+   printf("Pascal       Stanford        370\n");
+   // end raw data printing
+   printf(".TE");
+}

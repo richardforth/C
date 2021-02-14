56782a29a8855db0df804da642547a9b02c84cb5
Richard Forth <richard.forth@gmail.com>
2021-02-14 02:14:46 +0000

    Update take_stdin.c

--- take_stdin.c
+++ take_stdin.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 int main() {
 
         char ch;
-        printf("\n--------------- BEGIN STDIN -------------------\n");
+        char data[4096];
+        printf("\n-------- BEGIN STDIN (end with ctrl-d) ----------\n");
         // code to get data from stdin
         while  ( (ch = fgetc(stdin)) != EOF)
         {
-                printf("%c", ch);
+                //printf("%c", ch);
+                strncat(data, &ch, 1);
         }
         printf("----------------- END STDIN ---------------------\n");
+
+        printf("Data Collected:\n");
+        printf("%s", data);
 
         return 0;
 }

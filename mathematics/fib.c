48c5192467fe63f787512b36a2b86c1f85c1be00
Richard Forth <richard.forth@gmail.com>
2021-04-03 15:24:26 +0100

    Create fib.c

---
+++ mathematics/fib.c
@@ -0,0 +1,49 @@
+/* fib.c
+ *
+ * print fibonacci sequence up to desired number
+ * Author: Richard A. Forth
+ */
+
+ #include <string.h>
+
+ void fibber(int t)
+ {
+     int n = 1;
+     int pn = 0;
+     int sum = 1;
+
+     while (sum <= t )
+     {
+
+        printf("  '%d',\n", sum);
+        pn = n;
+        n = sum;
+        sum = n+pn;
+     }
+
+ }
+
+ int main()
+ {
+     int tgt;
+
+
+     printf("Enter target number to go up to: ");
+     scanf("%d", &tgt);
+     if (tgt > 1)
+     {
+         printf("Input received: %d", tgt);
+         printf("\nOutput:\n\n");
+         printf("[\n");
+         fibber(tgt);
+         printf("]\n\n");
+         printf("Done.");
+     }
+     else
+     {
+         printf("\nERROR! Input must be greater than 1.\n");
+     }
+
+
+     return 0;
+ }

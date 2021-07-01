aa01ce9fc94424d31adf6722910b2c7b61cb7845
Richard Forth <richard.forth@gmail.com>
2021-07-02 00:05:46 +0100

    Update csv2table.c

--- tbl_nroff/csv2table.c
+++ tbl_nroff/csv2table.c
@@ -4,35 +4,40 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define BUFSIZE 128
+
 // attribution: https://cmd.inp.nsk.su/old/cmd2/manuals/unix/UNIX_Unleashed/ch08.htm
 
 // cool demo to try out:
-// $ cat mydata.csv
-// Example CSV Data for conversion
-// Col1,Col2,Col3,Col4,Col5
-// Data1,Data2,Data3,Data4,Data5
-// $
-// $
-// $
-// $ ./csv2table mydata.csv 5 | tbl | nroff -Tascii | grep -v ^$
-// +--------------------------------------+
-// |   Example CSV Data for conversion    |
-// +------+-------+-------+-------+-------+
-// |Col1  | Col2  | Col3  | Col4  | Col5  |
-// +------+-------+-------+-------+-------+
-// |Data1 | Data2 | Data3 | Data4 | Data5 |
-// +------+-------+-------+-------+-------+
+/*
 
+$ cat mydata.csv
+Example CSV Data for conversion
+Col1,Col2,Col3,Col4,Col5
+Data1,Data2,Data3,Data4,Data5
+$
+$
+$
+$ ./csv2table mydata.csv 5
++--------------------------------------+
+|   Example CSV Data for conversion    |
++------+-------+-------+-------+-------+
+|Col1  | Col2  | Col3  | Col4  | Col5  |
++------+-------+-------+-------+-------+
+|Data1 | Data2 | Data3 | Data4 | Data5 |
++------+-------+-------+-------+-------+
+$
+
+*/
 
 
 int main ( int argc, char *argv[] )
 {
    int index;
-   FILE *fp;
+   FILE *fp, *tmpfp;
    char ch;
    char *fname;
    int ncols, i;
-   char mytitle[128];
 
    // get file name argument
    if (argc == 3 )
@@ -53,39 +58,59 @@ int main ( int argc, char *argv[] )
         return 1;
    }
    fp = fopen(fname, "r");
+   tmpfp = fopen("temp.dat", "w");
 
-   printf("\\\" This file produced by a program!\n");
-   printf(".ll 15i \n");
-   printf(".pl 200 \n");
-   printf(".TS \n");
-   printf("allbox;\n");
-   printf("c ");
+   fprintf(tmpfp, "\\\" This file produced by a program!\n");
+   fprintf(tmpfp, ".ll 15i \n");
+   fprintf(tmpfp, ".pl 200 \n");
+   fprintf(tmpfp, ".TS \n");
+   fprintf(tmpfp, "allbox;\n");
+   fprintf(tmpfp, "c ");
    for(i=1;i<=ncols-1;i++)
    {
-        printf("s ");
+        fprintf(tmpfp, "s ");
    }
-   printf("\n");
+   fprintf(tmpfp, "\n");
    for(i=1;i<=ncols;i++)
    {
-        printf("l ");
+        fprintf(tmpfp, "l ");
    }
-   printf(".\n");
-   // do something to print tab or space delimited data
+   fprintf(tmpfp, ".\n");
+   // convert csv data to tabbed data
+   // write to a new temporary file
    while  ( (ch = fgetc(fp)) != EOF)
     {
         if (ch == ',')
         {
-                printf("\t");
+                fprintf(tmpfp, "\t");
         }
         else
         {
-                printf("%c",ch);
+                fprintf(tmpfp, "%c",ch);
         }
     }
+   // end raw data printing
+   fprintf(tmpfp, ".TE");
+   fclose(tmpfp);
+   /* execute code against the temporary dat file */
+   char *cmd = "cat temp.dat | tbl | nroff -Tascii | grep -v ^$";
+   char buf[BUFSIZE];
+   FILE *cmdfp;
+   if ((cmdfp = popen(cmd, "r")) == NULL) {
+        printf("Error opening pipe!\n");
+        return -1;
+   }
+
+   while (fgets(buf, BUFSIZE, cmdfp) != NULL) {
+         printf("%s", buf);
+    }
+
+   if(pclose(cmdfp))  {
+       printf("Command not found or exited with error status\n");
+   }
+
    /* always close files you've opened */
    fclose(fp);
-   // end raw data printing
-   printf(".TE");
 
    return 0;
 }

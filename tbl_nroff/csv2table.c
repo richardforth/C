5722bf2885915c7e07c71906be60648b986224e5
Richard Forth <richard.forth@gmail.com>
2021-07-01 23:04:19 +0100

    Update and rename csv2table to csv2table.c

---
+++ tbl_nroff/csv2table.c
@@ -0,0 +1,91 @@
+/* csv2table */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// attribution: https://cmd.inp.nsk.su/old/cmd2/manuals/unix/UNIX_Unleashed/ch08.htm
+
+// cool demo to try out:
+// $ cat mydata.csv
+// Example CSV Data for conversion
+// Col1,Col2,Col3,Col4,Col5
+// Data1,Data2,Data3,Data4,Data5
+// $
+// $
+// $
+// $ ./csv2table mydata.csv 5 | tbl | nroff -Tascii | grep -v ^$
+// +--------------------------------------+
+// |   Example CSV Data for conversion    |
+// +------+-------+-------+-------+-------+
+// |Col1  | Col2  | Col3  | Col4  | Col5  |
+// +------+-------+-------+-------+-------+
+// |Data1 | Data2 | Data3 | Data4 | Data5 |
+// +------+-------+-------+-------+-------+
+
+
+
+int main ( int argc, char *argv[] )
+{
+   int index;
+   FILE *fp;
+   char ch;
+   char *fname;
+   int ncols, i;
+   char mytitle[128];
+
+   // get file name argument
+   if (argc == 3 )
+   {
+        fname = argv[1];
+        ncols = atoi(argv[2]);
+        if( ! access( fname, R_OK ) == 0 ) {
+                // file doesn't exist
+                printf("\nError: File won't open or doesnt exist: %s\n", fname);
+                return 1;
+        }
+   }
+   else
+   {
+        printf("\nError: Wrong number of arguments supplied!\n");
+        printf("Usage: ./rfmktable_multicol <filename> <No of Cols> | tbl | nroff\n");
+        printf("Example: ./rfmktable_multicol tab_data 6  | tbl | nroff\n");
+        return 1;
+   }
+   fp = fopen(fname, "r");
+
+   printf("\\\" This file produced by a program!\n");
+   printf(".ll 15i \n");
+   printf(".pl 200 \n");
+   printf(".TS \n");
+   printf("allbox;\n");
+   printf("c ");
+   for(i=1;i<=ncols-1;i++)
+   {
+        printf("s ");
+   }
+   printf("\n");
+   for(i=1;i<=ncols;i++)
+   {
+        printf("l ");
+   }
+   printf(".\n");
+   // do something to print tab or space delimited data
+   while  ( (ch = fgetc(fp)) != EOF)
+    {
+        if (ch == ',')
+        {
+                printf("\t");
+        }
+        else
+        {
+                printf("%c",ch);
+        }
+    }
+   /* always close files you've opened */
+   fclose(fp);
+   // end raw data printing
+   printf(".TE");
+
+   return 0;
+}

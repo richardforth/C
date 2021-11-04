6912434d052cc86d66c87f8fba66322c90ce1206
Richard Forth <richard.forth@gmail.com>
2021-11-04 09:49:47 +0000

    Update csv2report.c

--- data_analysis/csv2report.c
+++ data_analysis/csv2report.c
@@ -26,5 +26,46 @@ Col4................DataR2C4
 Col5................DataR2C5
 ...and so on
 
+
+....or....
+
+---
+Col1:
+  DataR1C1
+  
+Col2:
+  DataR1C2
+  
+Col3:
+  DataR1C3
+
+Col4:
+  DataR1C4
+
+Col5:
+  DataR1C5
+
+
+---
+Col1
+  DataR2C1
+
+Col2
+  DataR2C2
+
+Col3
+  DataR2C3
+
+Col4
+  DataR2C4
+
+Col5
+  DataR2C5
+
+
+...and so on....
+
+or maybe a choice of options?
+
 */
 

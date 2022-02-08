258067c0878073f400fd938aa2e2411d93b2d7c0
Richard Forth <richard.forth@gmail.com>
2022-02-08 14:32:15 +0000

    Update argsopts.c

--- argsopts.c
+++ argsopts.c
@@ -1,3 +1,45 @@
-/* argsopts.c*/
+/* argsopts.c
+ *
+ *   Author: Richard A. Forth
+ *   Does:   A simple script to test arguments and options
+ *   Date:   08-02-2022
+ *   Comments:
+ *           None
+ *
+ */
 
-tba
+#include <stdio.h>
+
+void displayHelp() {
+
+        printf("progname -h");
+}
+
+
+int main(int argc, char* argv[]) {
+
+        int i;
+
+        for (i = 1; i < argc; i++)
+        {
+                printf("argv[%u] = %s\n", i, argv[i]);
+                if (argv[i][0] == '-')
+                {
+                        if (argv[i][1] == 'h')
+                        {
+                                displayHelp();
+                        }
+                        else
+                        {
+                                printf("Invalid option.");
+                                return 2;
+                        }
+                }
+                else
+                {
+                        displayHelp();
+                }
+        }
+        return 0;
+
+}
